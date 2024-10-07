716bf5a6e1736d112f90367242c4d63dbd2e97ce
yohanii <yohanii@github.com>
2024-10-07 17:38:24 +0900

    feat: initial commit

---
+++ src/main/java/com/ktb10/munggaebe/MunggaebeApplication.java
@@ -0,0 +1,13 @@
+package com.ktb10.munggaebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunggaebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunggaebeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb10/munggaebe/MunggaebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb10.munggaebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunggaebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
