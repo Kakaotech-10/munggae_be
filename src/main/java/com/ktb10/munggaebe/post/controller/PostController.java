eb5998359b70be81657b556d78c295cffd0cc2a2
yohanii <yohanii@github.com>
2024-10-10 09:38:21 +0900

    feat: 여러 게시물 조회 시 최신순 정렬

--- src/main/java/com/ktb10/munggaebe/post/controller/PostController.java
+++ src/main/java/com/ktb10/munggaebe/post/controller/PostController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class PostController {
     public ResponseEntity<?> getPosts(@RequestParam(required = false, defaultValue = "0") final int pageNo,
                                        @RequestParam(required = false, defaultValue = "10") final int pageSize) {
 
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by("createdAt").descending());
         Page<Post> posts = postService.getPosts(pageable);
 
         return ResponseEntity.ok(posts);
