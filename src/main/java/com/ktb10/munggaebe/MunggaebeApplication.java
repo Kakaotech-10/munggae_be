ec406af1dc298fc5a5c017a6ce8796b2527cbf76
yohanii <yohanii@github.com>
2024-10-16 11:48:23 +0900

    chore: OpenFeign 관련 설정

--- src/main/java/com/ktb10/munggaebe/MunggaebeApplication.java
+++ src/main/java/com/ktb10/munggaebe/MunggaebeApplication.java
@@ -2,8 +2,10 @@ package com.ktb10.munggaebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MunggaebeApplication {
 
 	public static void main(String[] args) {
