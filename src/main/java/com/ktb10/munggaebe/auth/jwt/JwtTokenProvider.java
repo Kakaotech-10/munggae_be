38f3a76d27b30a846dca185f973628c258302264
yohanii <yohanii@github.com>
2024-10-16 16:48:13 +0900

    feat: JWT Token 방식 로그인 구현

--- src/main/java/com/ktb10/munggaebe/auth/jwt/JwtTokenProvider.java
+++ src/main/java/com/ktb10/munggaebe/auth/jwt/JwtTokenProvider.java
@@ -1,8 +1,38 @@
 package com.ktb10.munggaebe.auth.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
 @Component
 public class JwtTokenProvider {
 
+    private Key key;
+
+    public JwtTokenProvider(@Value("${custom.jwt.secretKey}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String accessTokenGenerate(String subject, Date expiredAt) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setExpiration(expiredAt)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+    public String refreshTokenGenerate(Date expiredAt) {
+        return Jwts.builder()
+                .setExpiration(expiredAt)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 }
--- src/main/java/com/ktb10/munggaebe/auth/jwt/TokenManager.java
+++ src/main/java/com/ktb10/munggaebe/auth/jwt/TokenManager.java
@@ -4,13 +4,26 @@ import com.ktb10.munggaebe.auth.dto.AuthTokens;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 @RequiredArgsConstructor
 public class TokenManager {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthTokens generate(String string) {
-        return null;
+    private static final String BEARER_TYPE = "Bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60;	           //1hour
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14;    //14days
+
+    public AuthTokens generate(String uid) {
+        long now = (new Date()).getTime();
+        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+
+        String accessToken = jwtTokenProvider.accessTokenGenerate(uid, accessTokenExpiredAt);
+        String refreshToken = jwtTokenProvider.refreshTokenGenerate(refreshTokenExpiredAt);
+
+        return AuthTokens.of(accessToken, refreshToken, BEARER_TYPE, ACCESS_TOKEN_EXPIRE_TIME / 1000L);
     }
 }
