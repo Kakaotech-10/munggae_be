92253f2ec66c651c95f3c3bb38b290d73663cbf2
yohanii <yohanii@github.com>
2024-11-04 17:38:10 +0900

    fix: api 응답값에 JsonProperty 추가

--- src/main/java/com/ktb10/munggaebe/post/client/dto/FilteringRes.java
+++ src/main/java/com/ktb10/munggaebe/post/client/dto/FilteringRes.java
@@ -1,14 +1,22 @@
 package com.ktb10.munggaebe.post.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
+@ToString
 public class FilteringRes {
 
+    @JsonProperty("origin_text")
     private String originText;
+
+    @JsonProperty("filtered_labels")
     private List<String> filteredLabels;
+
+    @JsonProperty("message")
     private String message;
 
     public FilteringRes(String originText, List<String> filteredLabels, String message) {
