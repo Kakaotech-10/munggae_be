b1b1f26795d3c641ad2424656e52fcec6e1d13e5
yohanii <yohanii@github.com>
2024-11-04 17:36:48 +0900

    fix: filterText 비동기로 변환 & uri 수정

--- src/main/java/com/ktb10/munggaebe/post/client/TextFilteringClient.java
+++ src/main/java/com/ktb10/munggaebe/post/client/TextFilteringClient.java
@@ -4,8 +4,8 @@ import com.ktb10.munggaebe.post.client.dto.FilteringReq;
 import com.ktb10.munggaebe.post.client.dto.FilteringRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
@@ -13,12 +13,13 @@ public class TextFilteringClient {
 
     private final WebClient fastApiWebClient;
 
-    public Mono<FilteringRes> filterText(String text) {
-        String uri = "comments";
+    public FilteringRes filterText(String text) {
+        String uri = "/comments/";
         return fastApiWebClient.post()
                 .uri(uri)
-                .body(new FilteringReq(text), FilteringReq.class)
+                .body(BodyInserters.fromValue(new FilteringReq(text)))
                 .retrieve()
-                .bodyToMono(FilteringRes.class);
+                .bodyToMono(FilteringRes.class)
+                .block();
     }
 }
