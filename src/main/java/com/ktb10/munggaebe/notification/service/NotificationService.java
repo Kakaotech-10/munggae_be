3209a1dd600785c321c7da6cb016f2de36b1647f
yohanii <yohanii@github.com>
2024-11-19 10:34:35 +0900

    feat: NotificationService.subscribe 구현

--- src/main/java/com/ktb10/munggaebe/notification/service/NotificationService.java
+++ src/main/java/com/ktb10/munggaebe/notification/service/NotificationService.java
@@ -1,13 +1,29 @@
 package com.ktb10.munggaebe.notification.service;
 
+import com.ktb10.munggaebe.notification.domain.SseEmitterRepository;
+import com.ktb10.munggaebe.utils.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
 
-    //구독한 맴버 레포지토리로 저장
+    private final SseEmitterRepository emitterRepository;
+
+    private static final long DEFAULT_TIMEOUT = 10 * 60 * 1000; //10m
+
     public SseEmitter subscribe(long lastEventId) {
-        return null;
+
+        SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
+
+        Long userId = SecurityUtil.getCurrentUserId();
+
+        emitter.onCompletion(() -> emitterRepository.deleteById(userId));
+        emitter.onTimeout(() -> emitterRepository.deleteById(userId));
+        emitter.onError((e) -> emitterRepository.deleteById(userId));
+
+        return emitterRepository.save(userId, emitter);
     }
 }
