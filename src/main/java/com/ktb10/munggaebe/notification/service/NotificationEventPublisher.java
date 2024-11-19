a12337914bf81fcec8d68404c159902cf0fb5e4b
yohanii <yohanii@github.com>
2024-11-19 16:18:26 +0900

    feat: NotificationEventPublisher 구현

--- src/main/java/com/ktb10/munggaebe/notification/service/NotificationEventPublisher.java
+++ src/main/java/com/ktb10/munggaebe/notification/service/NotificationEventPublisher.java
@@ -1,8 +1,35 @@
 package com.ktb10.munggaebe.notification.service;
 
+import com.ktb10.munggaebe.notification.domain.NotificationType;
+import com.ktb10.munggaebe.notification.service.dto.NotificationEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationEventPublisher {
-    //다른 도메인에서 publisher를 통해, 알람 생성 후 메시지 큐에 publish
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private static final String NOTIFICATION_TOPIC = "notifications";
+
+    public NotificationEvent createBroadCastingEvent(NotificationType notificationType, String message) {
+        return NotificationEvent.builder()
+                .type(notificationType)
+                .message(message)
+                .build();
+    }
+
+    public NotificationEvent createUniCastingEvent(Long receiverId, NotificationType notificationType, String message) {
+        return NotificationEvent.builder()
+                .receiverId(receiverId)
+                .type(notificationType)
+                .message(message)
+                .build();
+    }
+
+    public void publishEvent(NotificationEvent event) {
+        redisTemplate.convertAndSend(NOTIFICATION_TOPIC, event);
+    }
 }
---
+++ src/main/java/com/ktb10/munggaebe/notification/service/dto/NotificationEvent.java
@@ -0,0 +1,20 @@
+package com.ktb10.munggaebe.notification.service.dto;
+
+import com.ktb10.munggaebe.notification.domain.NotificationType;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class NotificationEvent {
+
+    private Long receiverId;
+    private NotificationType type;
+    private String message;
+
+    @Builder
+    public NotificationEvent(Long receiverId, NotificationType type, String message) {
+        this.receiverId = receiverId;
+        this.type = type;
+        this.message = message;
+    }
+}
