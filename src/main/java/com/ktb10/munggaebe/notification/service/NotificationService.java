0c78e05a2296c7dbcbd068d019afa308189dd8b5
yohanii <yohanii@github.com>
2024-11-19 13:13:19 +0900

    docs: 주석 추가

--- src/main/java/com/ktb10/munggaebe/notification/service/NotificationService.java
+++ src/main/java/com/ktb10/munggaebe/notification/service/NotificationService.java
@@ -1,6 +1,6 @@
 package com.ktb10.munggaebe.notification.service;
 
-import com.ktb10.munggaebe.notification.domain.SseEmitterRepository;
+import com.ktb10.munggaebe.notification.repository.SseEmitterRepository;
 import com.ktb10.munggaebe.utils.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,6 +23,8 @@ public class NotificationService {
         emitter.onCompletion(() -> emitterRepository.deleteById(userId));
         emitter.onTimeout(() -> emitterRepository.deleteById(userId));
         emitter.onError((e) -> emitterRepository.deleteById(userId));
+
+        //NotificationRepository에서 lastEventId 이후의 notifications를 해당 유저에게 다시 전송
 
         return emitterRepository.save(userId, emitter);
     }
