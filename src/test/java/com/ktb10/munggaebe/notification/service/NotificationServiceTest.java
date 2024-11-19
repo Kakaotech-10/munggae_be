d5008a9f2f4423f37dc87a4449a7b0ef26383f56
yohanii <yohanii@github.com>
2024-11-19 10:34:03 +0900

    test: 비동기 테스트 제거

--- src/test/java/com/ktb10/munggaebe/notification/service/NotificationServiceTest.java
+++ src/test/java/com/ktb10/munggaebe/notification/service/NotificationServiceTest.java
@@ -1,21 +1,25 @@
 package com.ktb10.munggaebe.notification.service;
 
+import com.ktb10.munggaebe.member.domain.Member;
+import com.ktb10.munggaebe.member.domain.MemberRole;
 import com.ktb10.munggaebe.notification.domain.SseEmitterRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
@@ -25,6 +29,19 @@ class NotificationServiceTest {
 
     @Mock
     private SseEmitterRepository emitterRepository;
+
+    @BeforeEach
+    void setup() {
+        Member member = Member.builder().id(1L).role(MemberRole.STUDENT).build();
+        setupSecurityContextWithRole(member, "STUDENT");
+    }
+
+    private void setupSecurityContextWithRole(Member member, String role) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                member, null, Collections.singleton(() -> "ROLE_" + role)
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 
     @Test
     @DisplayName("구독한 사용자의 emitter를 성공적으로 저장한다.")
@@ -59,63 +76,5 @@ class NotificationServiceTest {
         // Then
         assertThat(result).isNotNull();
         assertThat(result.getTimeout()).isEqualTo(defaultTimeout);
-    }
-
-    @Test
-    @DisplayName("emitter 완료 시, 저장소에서 삭제한다.")
-    void subscribe_delete_whenCompleted() {
-        // Given
-        long lastEventId = 1L;
-        SseEmitter emitter = new SseEmitter(1L);
-
-        given(emitterRepository.save(anyLong(), any())).willReturn(emitter);
-
-        // When
-        SseEmitter result = notificationService.subscribe(lastEventId);
-        result.complete();
-
-        // Then
-        assertThat(result).isNotNull();
-        verify(emitterRepository).deleteById(anyLong());
-    }
-
-    @Test
-    @DisplayName("emitter 타임아웃 시, 저장소에서 삭제한다.")
-    void subscribe_delete_whenTimeout() throws InterruptedException {
-        // Given
-        long lastEventId = 1L;
-        SseEmitter emitter = new SseEmitter(1L);
-
-        given(emitterRepository.save(anyLong(), any())).willReturn(emitter);
-
-        // When
-        SseEmitter result = notificationService.subscribe(lastEventId);
-        Thread.sleep(5);
-
-        // Then
-        assertThat(result).isNotNull();
-        verify(emitterRepository).deleteById(lastEventId);
-    }
-
-    @Test
-    @DisplayName("emitter 에러 발생 시, 저장소에서 삭제한다.")
-    void subscribe_delete_whenError() {
-        // Given
-        long lastEventId = 1L;
-        SseEmitter emitter = new SseEmitter(1L);
-
-        given(emitterRepository.save(anyLong(), any())).willReturn(emitter);
-
-        // When
-        SseEmitter result = notificationService.subscribe(lastEventId);
-
-        try {
-            result.send(new Object());
-        } catch (IOException ignored) {
-        }
-
-        // Then
-        assertThat(result).isNotNull();
-        verify(emitterRepository).deleteById(lastEventId);
     }
 }
