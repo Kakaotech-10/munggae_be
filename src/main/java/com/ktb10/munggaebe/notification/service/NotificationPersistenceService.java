140a60904e7b88759751980a0d55b9368bea5d11
yohanii <yohanii@github.com>
2024-11-21 16:10:14 +0900

    feat: NotificationPersistenceService log 추가

--- src/main/java/com/ktb10/munggaebe/notification/service/NotificationPersistenceService.java
+++ src/main/java/com/ktb10/munggaebe/notification/service/NotificationPersistenceService.java
@@ -6,9 +6,11 @@ import com.ktb10.munggaebe.member.repository.MemberRepository;
 import com.ktb10.munggaebe.notification.repository.NotificationRepository;
 import com.ktb10.munggaebe.notification.service.dto.NotificationEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -19,11 +21,13 @@ public class NotificationPersistenceService {
 
     @Transactional
     public void saveBroadCasting(NotificationEvent event) {
+        log.info("saveBroadCasting start");
         notificationRepository.save(event.toEntity());
     }
 
     @Transactional
     public void saveUnicasting(NotificationEvent event) {
+        log.info("saveUnicasting start: receiverId = {}", event.getReceiverId());
         Member member = findMemberByReceiverId(event.getReceiverId());
         notificationRepository.save(event.toEntity(member));
     }
