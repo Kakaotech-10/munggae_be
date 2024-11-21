d4bfa74fe35d06b6ccb27687a148261e667545e1
yohanii <yohanii@github.com>
2024-11-21 15:42:43 +0900

    feat: swagger api 문서화 코드 추가

--- src/main/java/com/ktb10/munggaebe/notification/controller/NotificationController.java
+++ src/main/java/com/ktb10/munggaebe/notification/controller/NotificationController.java
@@ -4,6 +4,9 @@ import com.ktb10.munggaebe.notification.domain.NotificationType;
 import com.ktb10.munggaebe.notification.service.NotificationEventPublisher;
 import com.ktb10.munggaebe.notification.service.NotificationService;
 import com.ktb10.munggaebe.notification.service.dto.NotificationEvent;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +19,15 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/notifications")
+@Tag(name = "Notification API", description = "알림 관련 API")
 public class NotificationController {
 
     private final NotificationService notificationService;
     private final NotificationEventPublisher publisher;
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @Operation(summary = "알림 SSE 연결", description = "알림 SSE 연결 시도합니다.")
+    @ApiResponse(responseCode = "200", description = "알림 SSE 연결 성공")
     public ResponseEntity<SseEmitter> subscribe(@RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
 
         SseEmitter emitter = notificationService.subscribe(lastEventId);
@@ -30,6 +36,8 @@ public class NotificationController {
     }
 
     @GetMapping(value = "/publish") // publish, consume 테스트 용 controller method
+    @Operation(summary = "(임시) 알림 전송 테스트", description = "(임시) 알림 전송 테스트합니다.")
+    @ApiResponse(responseCode = "200", description = "(임시) 알림 전송 테스트 성공")
     public ResponseEntity<?> publish() {
         publisher.publishEvent(NotificationEvent.builder()
                 .receiverId(null)
