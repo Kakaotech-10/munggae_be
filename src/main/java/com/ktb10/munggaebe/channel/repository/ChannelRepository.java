deeb956ed818cd8a7ae2843993e957ab7e5e3e95
rimeir <skfsk1602@gmail.com>
2024-12-05 17:31:02 +0900

    feat: ChannelRepository 데이터베이스 상호작용 구현

---
+++ src/main/java/com/ktb10/munggaebe/channel/repository/ChannelRepository.java
@@ -0,0 +1,7 @@
+package com.ktb10.munggaebe.channel.repository;
+
+import com.ktb10.munggaebe.channel.domain.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+}
