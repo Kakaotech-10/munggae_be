15bee1fb9e097c244b0a76cdfc73179e6cf29683
rimeir <skfsk1602@gmail.com>
2024-12-06 13:38:29 +0900

    feat: MemberChannel 엔터티 구현

---
+++ src/main/java/com/ktb10/munggaebe/channel/domain/MemberChannel.java
@@ -0,0 +1,40 @@
+package com.ktb10.munggaebe.channel.domain;
+
+import com.ktb10.munggaebe.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberChannel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_channel_id", updatable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
+
