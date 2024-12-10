6ecd39d3a1da706af77a3830f23c781d5ed3e2ac
yohanii <yohanii@github.com>
2024-12-10 17:40:12 +0900

    feat: keyword 랭킹 기능 skeleton 작성

---
+++ src/main/java/com/ktb10/munggaebe/keyword/controller/KeywordController.java
@@ -0,0 +1,11 @@
+package com.ktb10.munggaebe.keyword.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class KeywordController {
+
+    //keyword ranking top3 get 요청
+}
---
+++ src/main/java/com/ktb10/munggaebe/keyword/service/KeywordService.java
@@ -0,0 +1,9 @@
+package com.ktb10.munggaebe.keyword.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KeywordService {
+
+    //ElasticSearch에서 keyword aggregate로 조회
+}
