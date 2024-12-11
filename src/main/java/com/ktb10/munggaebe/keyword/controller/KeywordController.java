439e98734f9d9ebfc0f3faebe1b22f478bb848f7
yohanii <yohanii@github.com>
2024-12-11 17:35:05 +0900

    feat: keyword ranking top 3 반환 기능 구현

--- src/main/java/com/ktb10/munggaebe/keyword/controller/KeywordController.java
+++ src/main/java/com/ktb10/munggaebe/keyword/controller/KeywordController.java
@@ -1,5 +1,6 @@
 package com.ktb10.munggaebe.keyword.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ktb10.munggaebe.keyword.controller.dto.KeywordDto;
 import com.ktb10.munggaebe.keyword.service.KeywordService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/keywords")
@@ -18,14 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class KeywordController {
 
     private final KeywordService keywordService;
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/ranking")
     @Operation(summary = "키워드 랭킹 탑3", description = "키워드 랭킹 탑3 조회해 반환합니다.")
     @ApiResponse(responseCode = "200", description = "키워드 랭킹 탑3 조회 성공")
     public ResponseEntity<KeywordDto.KeywordRankingTopThreeRes> keywordRankingTopThree() {
 
-        keywordService.getKeywordRankingTopThree();
+        List<String> keywords = keywordService.getKeywordRankingTopThree();
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new KeywordDto.KeywordRankingTopThreeRes(keywords));
     }
 }
