547966d4cebe4e9f64fab25d97720a5278328690
yohanii <yohanii@github.com>
2024-12-11 17:35:05 +0900

    feat: keyword ranking top 3 반환 기능 구현

--- src/main/java/com/ktb10/munggaebe/keyword/service/KeywordService.java
+++ src/main/java/com/ktb10/munggaebe/keyword/service/KeywordService.java
@@ -14,8 +14,15 @@ public class KeywordService {
 
     private final ElasticsearchKeywordService elasticsearchKeywordService;
 
-    public void getKeywordRankingTopThree() {
+    public List<String> getKeywordRankingTopThree() {
         List<Map.Entry<String, Long>> keywordRankings = elasticsearchKeywordService.getTopKeywords(3);
         log.info("Top 3 keyword rankings : {}", keywordRankings);
+
+        List<String> keywords = keywordRankings.stream()
+                .map(Map.Entry::getKey)
+                .toList();
+        log.info("Top 3 keywords : {}", keywords);
+
+        return keywords;
     }
 }
