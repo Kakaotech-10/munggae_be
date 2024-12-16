194c696df96314fe94673089902a3a9f5aa79400
yohanii <yohanii@github.com>
2024-12-16 16:18:28 +0900

    feat: 게시물 목록 조회 시, 현재시간 이전의 createdAt을 가진 게시물 조회하도록 수정

--- src/main/java/com/ktb10/munggaebe/post/repository/PostRepository.java
+++ src/main/java/com/ktb10/munggaebe/post/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.ktb10.munggaebe.post.repository;
 
 import com.ktb10.munggaebe.post.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByCreatedAtBefore(LocalDateTime time, Pageable pageable);
 }
