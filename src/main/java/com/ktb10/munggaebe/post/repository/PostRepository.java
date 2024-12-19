7f2021d54feb9f9bbdf494d437487405127ef20f
rimeir <skfsk1602@gmail.com>
2024-12-19 19:10:05 +0900

    feat: channelId 조건을 포함한 게시글 조회 쿼리 구현

--- src/main/java/com/ktb10/munggaebe/post/repository/PostRepository.java
+++ src/main/java/com/ktb10/munggaebe/post/repository/PostRepository.java
@@ -14,4 +14,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByCreatedAtBefore(LocalDateTime time, Pageable pageable);
 
     Page<Post> findByChannelIdAndDeadLineIsNotNull(Long channelId, Pageable pageable);
+
+    Page<Post> findByChannelIdAndCreatedAtBefore(Long channelId, LocalDateTime time, Pageable pageable); //channelId 조건을 포함한 게시글 조회 쿼리
 }
